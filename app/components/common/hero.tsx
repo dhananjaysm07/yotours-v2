9cddb4b53f37af52b567650f20415944754c1441
Suryansh <suryanshmishra.0904@gmail.com>
2025-02-24 19:39:10 +0530

    fix: performance check [1]

--- app/components/common/hero.tsx
+++ app/components/common/hero.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import FadeUpAnimation from './fade-up';
+// import FadeUpAnimation from './fade-up';
 import AppSearch from '../app-search/app-search';
 import { GetDestinations } from '@/types';
 
@@ -32,25 +32,25 @@ const Hero = ({
         <div className="row justify-center">
           <div className="col-auto">
             <div className="text-center">
-              <FadeUpAnimation delay={0.1}>
-                <h1 className="text-60 lg:text-40 md:text-30 text-white">
-                  {contentData?.heroHeading || 'Find Next Place To Visit'}
-                </h1>
-              </FadeUpAnimation>
+              {/* <FadeUpAnimation delay={0.1}> */}
+              <h1 className="text-60 lg:text-40 md:text-30 text-white">
+                {contentData?.heroHeading || 'Find Next Place To Visit'}
+              </h1>
+              {/* </FadeUpAnimation> */}
 
-              <FadeUpAnimation delay={0.3}>
-                <p className="text-white mt-6 md:mt-10 text-25">
-                  {contentData?.heroSubheading ||
-                    'Discover amazing places at exclusive deals'}
-                </p>
-              </FadeUpAnimation>
+              {/* <FadeUpAnimation delay={0.3}> */}
+              <p className="text-white mt-6 md:mt-10 text-25">
+                {contentData?.heroSubheading ||
+                  'Discover amazing places at exclusive deals'}
+              </p>
+              {/* </FadeUpAnimation> */}
             </div>
 
-            <FadeUpAnimation delay={0.4}>
-              <div className="tabs -underline mt-60 js-tabs">
-                <AppSearch destinations={destinations} />
-              </div>
-            </FadeUpAnimation>
+            {/* <FadeUpAnimation delay={0.4}> */}
+            <div className="tabs -underline mt-60 js-tabs">
+              <AppSearch destinations={destinations} />
+            </div>
+            {/* </FadeUpAnimation> */}
           </div>
         </div>
       </div>
