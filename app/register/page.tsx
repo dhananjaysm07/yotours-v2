3df0f85a747b708f0e3c992b12ed46cdec8ad5f3
Pawan608 <agarwalpawan6089@gmail.com>
2025-07-25 23:15:09 +0530

    feat:register

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -88,12 +88,6 @@ export default function Register() {
     >
       <div className="col-12">
         <h1 className="text-22 fw-500">Create Account</h1>
-        <p className="mt-10">
-          Already have an account?{" "}
-          <Link href="/login" className="text-blue-1">
-            Sign in here
-          </Link>
-        </p>
       </div>
 
       {error && (
